483e6aa8e3d75c5a12753bf3b7c0da8b91d58992
Tim Yager <tyager@clickhere.com>
2012-11-19 17:27:59 -0600

    Filtering mostly done and step through begun.

--- grv-selector/selector.js
+++ grv-selector/selector.js
@@ -1,33 +1,89 @@
-// A B C TT 5thW EXP FCT TC
+function StepThrough(element) {
+  this.$element = element = $(element);
+
+  this.steps = {
+    '$splash': $('#splash'),
+    '$lifestyles': $('#lifestyles'),
+    '$selector': $('#selector'),
+    '$results': $('#results')
+  };
+
+	// setup event listeners
+	this._setupEventListeners();
+
+};
+
+StepThrough.prototype = {
+  _setupEventListeners: function () {
+		var $element = this.$element;
+
+		$(window).on('load', $.proxy(this, 'showSplash'));
+		
+		$element.on('click', '#open-lifestyles', $.proxy(this, 'showLifestyles'));
+		$element.on('click', '#open-selector', $.proxy(this, 'showSelector'));
+		$element.on('click', '#open-results', $.proxy(this, 'showSelector'));
+  },
+  showSplash: function() {
+    console.log('SHOW SPLASH');
+    this.resetPanels();
+    this.steps.$splash.addClass('open');
+  },
+  showLifestyles: function() {
+    console.log('SHOW LIFESTYLES');
+    this.resetPanels();
+    this.steps.$lifestyles.addClass('open');
+  },
+  showSelector: function() {
+    console.log('SHOW SELECTOR');
+    this.resetPanels();
+    this.steps.$selector.addClass('open');
+    var selector = new RVSelector(this.steps.$selector);
+  },
+  showResults: function() {
+    console.log('SHOW RESULTS');
+    this.resetPanels();
+    this.steps.$results.addClass('open');
+  },
+  resetPanels: function() {
+    this.$element.children().removeClass('open');
+  }
+};
 
 function RVSelector(element) {
-  // setup intial properties
+  // setup selectors
+	this.$element = element = $(element);
+	this.$currentQuestionCounter = element.find('.current-question span');
+	this.$products = this.$element.find('.grid li');
+	this.$results = $('#results .rv');
+
+	// setup properties
 	this.currentQuestion = 1;
 	this.currentQuestionCount = 1;
-	
-	this.$element = element = $(element);
+	this.answerSet = {}
 	this.products = element.find('.rv-info');
 	this.questions = element.find('.question');
-	this.$currentQuestionCounter = element.find('.current-question span');
+	this.productList = 'A B C TT 5thW EXP FCT TC'.split(' ');
+	this.activeProducts = new Array();
 
 	// setup event listeners
 	this._setupEventListeners();
 
 	// initialize the tool
 	this.startOver();
+
 }
 
 RVSelector.prototype = {
 	_setupEventListeners: function () {
-		var element = this.$element;
-		
-		element.on('change', ':radio', $.proxy(this, 'onAnswerChange'));
-		element.on('click', 'button.next', $.proxy(this, 'next'));
-		element.on('click', 'button.previous', $.proxy(this, 'previous'));
-		
+		var $element = this.$element;
+
+		$element.on('change', ':radio', $.proxy(this, 'onAnswerChange'));
+		$element.on('click', 'button.next', $.proxy(this, 'next'));
+		$element.on('click', 'button.previous', $.proxy(this, 'previous'));
+
 		$(window).on('unload', $.proxy(this, 'showWarning'));
 	},
-	
+
 	onAnswerChange: function (e) {
 		var didYouKnow = $(e.target).data('did-you-know');
 
@@ -36,88 +92,157 @@ RVSelector.prototype = {
 			this.updateDidYouKnow(didYouKnow);
 		}
 
+		// uncheck any answers to questions we're not on yet
+		this.$element.find('.question :checked :gt(' + (this.currentQuestion-1) + ')').prop('checked', false);
+
+		// update the grid
 		this.filterResults();
+
 	},
-	
+
 	filterResults: function () {
 	  console.log('CALL: filterResults()');
+	  console.log('CURRENT QUESTION INDEX: ' + this.currentQuestion);
 
-		var $question = this.getCurrentQuestion();
-		var result = $question.find(':checked').data('results');
-		
-		if (!!result) {
-		
-		  /*
-		   * Ok, I need to gather all the questions that have been aswered
-		   *   Disable all questions
-		   *   Loop through each answer
-		   *       Remove 'disabled' class for each item in the data-results
-		   *
-		   */
-		
-		
-		  console.log('RESULT: ' + result);
-		  console.log(typeof result);
-  		//var results = $.map(result.split(' '), function (id) { return id; }).join(',');
-  		var results = result.split(' ');
-  		//console.log(typeof results);
-  		//console.log(results.splice());
-  		//console.log($('.grid ul li').not('li[data-code="TT"]'));
-  		$('.grid ul li').each(function(){
-    		$(this).addClass('disabled');
-  		});
-  		$.each(results, function(index){
-    		$('.grid ul li[data-code="' + results[index] + '"]').attr('class', '');
-  		});
+	  // only grab results for answers for the current question and before
+		var answers = this.$element.find('.question :checked :lt(' + this.currentQuestion + ')');
+		this.resetProducts();
 
-  		// filter the grid
-  		
-  		
-  		// filter the results panel
-  		//this.products.filter(':not(' + resultsSelector + ')').addClass('disabled');
-		}
+		// if we have answers we should filter
+		if (!!answers.length) {
+		  this.disableProducts();
+
+		  // gather all the results into a set
+		  var answerSet = new Array();
+		  var newActiveProducts = new Array();
+		  var data;
+
+		  // create a temp variable to point to the this scope
+		  var that = this;
+
+		  // iterate through each answer
+		  $.each(answers, function(index) {
+		    // store the products to keep active
+  		  data = $(answers[index]).data('results').split(' ');
+  		  answerSet.push(data);
+
+  		  // IF this is the first question
+  		  // OR there aren't any active products
+  		  // THEN set activeProducts to the current set of answers (should always be the first set)
+  		  if (that.currentQuestion == 1 || !that.activeProducts.length) {
+    		  // ADD
+    		  that.activeProducts = answerSet[index];
+
+  		  } else {
+    		  // ELIMINATE
+    		  $.each(that.activeProducts, function(k) {
+      		  if ($.inArray(that.activeProducts[k], answerSet[index]) >= 0) {
+        		  newActiveProducts.push(that.activeProducts[k]);
+      		  }
+    		  });
+    		  that.activeProducts = newActiveProducts;
+    		  newActiveProducts = new Array();
+
+  		  } // if (that.currentQuestion == 1 || !that.activeProducts.length) {
+
+		  }); // $.each(answers, function(index) {
+
+		  // iterate through the set removing disabled classes on grid
+		  this._debugAnswerSetReset();
+		  this.disableProducts();
+		  this.showActiveProducts(this.activeProducts);
+		  this.updateResultsPage(this.activeProducts);
+
+		  // reset this.activeProducts so we're always starting clean
+		  this.activeProducts = new Array();
+
+		} // if (!!answers)
 
 	},
-	
+
+	showActiveProducts: function(products) {
+	  var that = this;
+
+    $.each(products, function(index) {
+  	  $('.grid li[data-code="' + products[index]  + '"]').removeClass('disabled');
+  	  that._debugAnswerSet(products[index]);
+    });
+
+	},
+	disableProducts: function() {
+  	this.$products.addClass('disabled');
+
+	},
+	resetProducts: function() {
+  	this.$products.attr('class', '');
+
+	},
+	resetResultsPage: function() {
+  	this.$results.removeClass('enabled');
+
+	},
+	_debugAnswerSet: function(string) {
+  	$('#debug .answer-set').append('<p>' + string + '</p>');
+
+	},
+	_debugAnswerSetReset: function() {
+  	$('#debug .answer-set').html('');
+
+	},
+
 	startOver: function () {
 		this.$element.find(':checked').prop('checked', false);
 		this.hideQuestions();
 		this.showQuestion(1);
+
 	},
-	
+
+	updateResultsPage: function(products) {
+  	console.log('CALL: updateResultsPage(products)');
+
+  	this.resetResultsPage();
+
+  	$.each(products, function(index) {
+    	$('#results .rv[data-code="' + products[index] + '"]').addClass('enabled');
+  	});
+
+	},
 	updateDidYouKnow: function (index) {
 	  console.log('CALL: updateDidYouKnow(' + index + ')');
-	  
+
 	  var nextDYK = $('.did-you-knows .item[data-id="' + index + '"]').get(0);
 
 	  if (!!nextDYK) {
   	  // hide all
   	  $('.did-you-knows .item').hide();
-  
+
   	  // show the proper did-you-know
   	  $('.did-you-knows .item[data-id="' + index + '"]').show(); 
 	  }
+
 	},
 	
 	hideQuestions: function() {
   	console.log('CALL: hideQuestions()');
   	this.questions.each(function(){ $(this).hide(); });
+
 	},
 	showQuestion: function (index) {
 	  console.log('CALL: showQuestion(' + index + ')');
 
+	  this.filterResults();
 		this.setCurrentQuestion(index); // index: 1-based index
-
 		this.hideQuestions();
 
 		var $question = this.getCurrentQuestion();
 		$question.show();
 
-		// display the associated did-you-know
 		this.updateDidYouKnow($question.data('did-you-know'));
+
 	},
 	getCurrentQuestion: function() {
   	return $('#q' + this.currentQuestion);
+
 	},
 	setCurrentQuestion: function(index) {
   	this.currentQuestion = index;
@@ -127,25 +252,25 @@ RVSelector.prototype = {
       	return 'first last';
     	}).addClass('first');
 
-  	} else if ((this.currentQuestion > 1) && (this.currentQuestion < this.questions.length + 1)) {
+  	} else if ((this.currentQuestion > 1) && (this.currentQuestion < this.questions.length)) {
     	this.$element.removeClass(function(){
       	return 'first last';
     	});
 
-  	} else if (this.currentQuestion == this.questions.length + 1) {
+  	} else if (this.currentQuestion == this.questions.length) {
     	this.$element.removeClass(function(){
       	return 'first last';
     	}).addClass('last');
+
   	} // if (this.currentQuestion == 1)
 
 	},
-	setCurrentQuestionCount: function(increment) {
-  	console.log('CALL: setCurrentQuestionCount(' + increment + ')');
-  	console.log(this.currentQuestionCount, this);
+	incrementCurrentQuestionCount: function(increment) {
+  	console.log('CALL: incrementCurrentQuestionCount(' + increment + ')');
 
   	this.currentQuestionCount = this.currentQuestionCount + increment;
-  	console.log(this.currentQuestionCount);
   	this.$currentQuestionCounter.html(this.currentQuestionCount);
+
 	},
 	next: function () {
 		console.log('CALL: next()');
@@ -158,8 +283,9 @@ RVSelector.prototype = {
   		// determine the (1-based)index of the next question
   		// .data('next-question') will return qN, where N = our (1-based) index
   		var nextQuestion = $answered.data('next-question').substr(1);
-  		this.setCurrentQuestionCount(1);
+  		this.incrementCurrentQuestionCount(1);
   		this.showQuestion(nextQuestion);
+
 		}
 	},
 	previous: function () {
@@ -168,15 +294,16 @@ RVSelector.prototype = {
 
 		do {
   		index = index - 1;
-
   		backedUp = $('#q' + index).find(':checked').get(0);
 
 		} while ( !!backedUp == false );
 
 		if (index >= 0) {
-		  this.setCurrentQuestionCount(-1);
+		  this.incrementCurrentQuestionCount(-1);
 			this.showQuestion(index);
+
 		}
+
 	},
 	
 	showWarning: function () {
@@ -185,5 +312,5 @@ RVSelector.prototype = {
 	}
 };
 
-
-var selector = new RVSelector('#selector');
+var step = new StepThrough('#grv-selector-container');
+//var selector = new RVSelector('#selector');
